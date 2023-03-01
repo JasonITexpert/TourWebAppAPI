89887dd0adf7e97fe574bf77faae70730ba36d1c
JasonITexpert <91292616+JasonITexpert@users.noreply.github.com>
2023-03-01 14:04:57 +0000

    Add project files.

---
+++ TourWebAppAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TourWebAppAPI.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+
+        }
+    }
+}
